2d57759370441a2e232135dd639f507934a18bee
kaido1337 <idealtertini@gmail.com>
2024-06-16 12:20:12 +0200

    beginn editing design

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const projects = [
+    {
+        title: "ATM Application Spring Boot",
+        link: "https://gitlab.com/itertini/projects/atm-spring-boot"
+    },
+    {
+        title: "Exercise Tracker Spring Boot",
+        link: "https://gitlab.com/itertini/freecodecamp/exercise-tracker-spring-boot"
+    },
+]
+
+function renderProjectElements() {
+    const projectsList = document.querySelector('#projects-list');
+
+}
