da3635ec88dc7355a1411a666cb2d7092f6403e0
kaido1337 <idealtertini@gmail.com>
2024-06-18 19:29:27 +0200

    updated UI

--- js/script.js
+++ js/script.js
@@ -1,15 +1,81 @@
+
 const projects = [
     {
-        title: "ATM Application Spring Boot",
-        link: "https://gitlab.com/itertini/projects/atm-spring-boot"
+        title: 'ATM Application',
+        description: 'The ATM API is a robust and scalable backend service designed using the Spring Boot framework. It provides essential functionalities required for managing ATM operations, ensuring seamless interactions between users and their bank accounts.',
+        link: 'https://gitlab.com/itertini/projects/atm-spring-boot',
+        techStack: ['java', 'springBoot', 'hibernate', 'lombok']
     },
     {
-        title: "Exercise Tracker Spring Boot",
-        link: "https://gitlab.com/itertini/freecodecamp/exercise-tracker-spring-boot"
+        title: 'Exercise Tracker',
+        description: 'This project is a Spring Boot implementation of the Node.js Exercise Tracker API project. You can find the original project on Exercise Tracker API on FreeCodeCamp.',
+        link: 'https://gitlab.com/itertini/freecodecamp/exercise-tracker-spring-boot',
+        techStack: ['html', 'css', 'javaScript', 'java', 'springBoot', 'hibernate', 'lombok']
+    },
+    {
+        title: 'PfuschDB',
+        description: 'A platform to offer crafts or to search for and contact suitable craftsmen for a specific job',
+        link: 'https://gitlab.com/itertini/codersbay-2.7/pfuschdb',
+        techStack: ['html', 'css', 'javaScript', 'vueJS', 'vuetify', 'java', 'springBoot', 'hibernate', 'lombok']
     },
 ]
 
-function renderProjectElements() {
-    const projectsList = document.querySelector('#projects-list');
-
+const techStackImages = {
+    html: './img/html.png',
+    css: './img/css3.png',
+    javaScript: './img/JavaScript.png',
+    vueJS: './img/Vue.png',
+    vuetify: './img/vuetify.png',
+    java: './img/java.png',
+    springBoot: './img/spring-boot.png',
+    hibernate: './img/hibernate.svg',
+    lombok: './img/lombok.png'
 }
+
+const title = document.getElementById('title');
+let i = 0
+
+function renderProjectElements() {
+    const projectsDiv = document.querySelector('#projects-row');
+
+    for (let project of projects) {
+        const projectEntry = document.createElement('div');
+        const techStackHtml = project.techStack.map(tech => {
+            const imgUrl = techStackImages[tech]
+            return `<img src=${imgUrl} class="tech-icon me-2" />`
+        }).join('')
+        projectEntry.innerHTML = `
+            <a href=${project.link} target="_blank">
+                <div class="card p-2 h-100">
+                    <div class="card-body">
+                        <h5 class="card-title py-3 fw-semibold">${project.title}</h5>
+                        <hr>
+                        <p class=" card-text pt-4">${project.description}</p>
+                    </div>
+                    <div class="card-footer">
+                      ${techStackHtml}
+                    </div>                
+                </div>
+            </a>
+        `
+        projectEntry.classList.add('col')
+        projectsDiv.append(projectEntry)
+    }
+}
+
+function typeWriter() {
+    const txt = 'Hey, I\'m Ideal';
+
+    if (i < txt.length) {
+        title.innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100);
+    }
+}
+
+document.addEventListener('load',
+    renderProjectElements(),
+    particlesJS.load('particles-js', '../config/particlesjs-config.json'), () => {
+        console.log('loaded')
+    },
+    typeWriter())
